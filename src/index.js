fce0bcb100b1219233b1af4c6af59de4d1a37e09
Your Namehuetclariss <you@example.com>
2022-08-31 14:28:06 +0100

    4th commit authorisation
    Login & logout set up with Auth0

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-jnfv6hxx.eu.auth0.com'
+      clientId='3RRhHIxiwGvu9ypeVPKK6knfz0k5EUBn'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
